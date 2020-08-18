0d09ccd586120106379696341867d1b9e873eeb1
Cynicalvoe <hadengw@gmail.com>
2020-08-18 13:58:22 -0600

    Finish Python I scrolls

--- src/app/scrolls/pageConstants.ts
+++ src/app/scrolls/pageConstants.ts
@@ -1,14 +1,17 @@
 export class Pages {
     public static categories = [
-        { id: "tech101",name: "Tech 101" },
         { id: "python1",name: "Python Programming I" }
     ];
 
     public static pages = [
-        { id: "computerBasics", category:"tech101", name: "Computer Basics", url:"../assets/scrolls/pages/tech101/computerBasics.md" },
+        { id: "computerBasics", category:"", name: "Computer Basics", url:"../assets/scrolls/pages/tech101/computerBasics.md" },
         { id: "python1intro", category:"python1", name: "Introduction", url:"../assets/scrolls/pages/python1/part1.md" },
         { id: "python1part2", category:"python1", name: "Data Types", url:"../assets/scrolls/pages/python1/part2.md" },
-        { id: "python1part3", category:"python1", name: "Variables", url:"../assets/scrolls/pages/python1/part3.md" }
+        { id: "python1part3", category:"python1", name: "Variables", url:"../assets/scrolls/pages/python1/part3.md" },
+        { id: "python1part4", category:"python1", name: "Handling Input", url:"../assets/scrolls/pages/python1/part4.md" },
+        { id: "python1part5", category:"python1", name: "Functions", url:"../assets/scrolls/pages/python1/part5.md" },
+        { id: "python1part6", category:"python1", name: "Conditionals", url:"../assets/scrolls/pages/python1/part6.md" },
+        { id: "python1part7", category:"python1", name: "Wrap-up Exercise", url:"../assets/scrolls/pages/python1/part7.md" }
     ];
 
     public static getID(id: string): number {
