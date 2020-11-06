690b1befc544ad2b0124b506c6d1f13b95676e05
Cynicalvoe <hadengw@gmail.com>
2020-11-06 16:31:08 -0700

    Add dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'personalsite';
+export class AppComponent implements OnInit {
+  theme = "light-theme bg-shapes";
+
+  ngOnInit() {
+    let hour = new Date().getHours();
+
+    if(hour <= 7 || hour >= 19) {
+      this.theme = "dark-theme bg-cogs";
+    }
+  }
 }
