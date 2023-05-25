13727726f16862c29b0659c658fd0ba940124a8d
Haden Watne <haden@hadenwatne.com>
2023-05-24 21:21:25 -0600

    Clean up new code & adjust UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HangarComponent } from './components/hangar/hangar.component';
 import { SocialButtonComponent } from './components/social-button/social-button.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, NotFoundComponent, HangarComponent, SocialButtonComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent, HangarComponent, SocialButtonComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
